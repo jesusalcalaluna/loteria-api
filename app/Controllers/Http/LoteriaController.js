3584c58e834288a478fdfd9f99e702032bebe365
MartinDeep13 <martin0013esp@hotmail.com>
2020-06-24 16:03:15 -0500

    cambios

--- app/Controllers/Http/LoteriaController.js
+++ app/Controllers/Http/LoteriaController.js
@@ -64,23 +64,6 @@ class LoteriaController {
 
         },2000);
 
-        function Timer(callback, delay) {
-          var timerId, start, remaining = delay;
-
-          this.pause = function() {
-              window.clearTimeout(timerId);
-              remaining -= new Date() - start;
-          };
-
-          this.resume = function() {
-              start = new Date();
-              window.clearTimeout(timerId);
-              timerId = window.setTimeout(callback, remaining);
-          };
-
-          this.resume();
-      }
-
     });
 
 
