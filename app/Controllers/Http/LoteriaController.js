8133c948b9548ef0e6c59ae3cbc750538c73b45d
DaniGadget18 <dani_181298@hotmail.com>
2020-06-26 00:00:14 -0500

    cambios

--- app/Controllers/Http/LoteriaController.js
+++ app/Controllers/Http/LoteriaController.js
@@ -3,16 +3,32 @@
 class LoteriaController {
 
 
+    static jugadores = {}
+
+
+
 
   static goPlay(socket,io){
     var cartas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54];
     var pausado = false;
     var intervalo ;
+
+
     console.log('Alguien entro al Socket');
 
 
-    socket.on('join_room', (room)=>{
+    socket.on('join_room', ({room, players})=>{
       socket.join(room)
+      //guardar en jugadores
+      if (this.jugadores[room]){
+        this.jugadores[room].push(players[0])
+      } else {
+        this.jugadores[room] = players;
+      }
+      console.log(this.jugadores);
+      io.sockets.to(room).emit('new-player',  this.jugadores[room]);
+      console.log(players);
+
       console.log('Alguien entro a la sala');
     });
 
@@ -20,6 +36,8 @@ class LoteriaController {
       socket.leave(room)
       console.log('Alguien salio de la sala');
     });
+
+
 
     socket.on('loteria', ({room,data})=>{
       io.sockets.to(room).emit('loteria',  data);
@@ -62,7 +80,7 @@ class LoteriaController {
                 cartas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54];
               }
 
-        },2000);
+        },3000);
 
     });
 
